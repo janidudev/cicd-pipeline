11649a63d34a5c5b834c298abc6e3a9acfd3184a
Janidu Dhakshitha Yapa <janidudhakshi222@gmail.com>
2026-03-01 16:34:13 +0530

    initial commit

---
+++ src/test/java/com/cicd/cicdproj/CicdProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.cicdproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
